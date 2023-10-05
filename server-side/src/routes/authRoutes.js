d2288937823a6bd7effe61bbdda070e64d5abb2b
itsaFan <steffansim@gmail.com>
2023-10-05 18:53:19 +0800

    add xRequestId on route

--- server-side/src/routes/authRoutes.js
+++ server-side/src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { register, login, getCurrentUserData } = require("../controllers/authController");
 const { verifyJWT } = require("../middlewares/verifyJWT");
+const { xRequestId } = require("../middlewares/xRequestId");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get('/me', verifyJWT, getCurrentUserData)
+router.get("/me", xRequestId, verifyJWT, getCurrentUserData);
 
 module.exports = router;
--- server-side/src/routes/leadRoutes.js
+++ server-side/src/routes/leadRoutes.js
@@ -2,15 +2,16 @@ const express = require("express");
 const { verifyJWT } = require("../middlewares/verifyJWT");
 const { checkRole } = require("../middlewares/checkRole");
 const { viewProjects, createTask, leaderViewCreatedTask, leaderViewAllCreatedTasks, updateTask, deleteTask } = require("../controllers/leadController");
+const { xRequestId } = require("../middlewares/xRequestId");
 
 const router = express.Router();
 
-router.get("/projects", verifyJWT, checkRole(["leader"]), viewProjects);
+router.get("/projects", xRequestId, verifyJWT, checkRole(["leader"]), viewProjects);
 // router.post("/tasks/:projectId", verifyJWT, checkRole(["leader"]), createTask);
-router.post("/tasks/add", verifyJWT, checkRole(["leader"]), createTask);
-router.get("/task/:taskId", verifyJWT, checkRole(["leader"]), leaderViewCreatedTask);
-router.get("/tasks", verifyJWT, checkRole(["leader"]), leaderViewAllCreatedTasks);
-router.put("/tasks/edit/:taskId", verifyJWT, checkRole(["leader"]), updateTask);
-router.delete("/tasks/delete/:taskId", verifyJWT, checkRole(["leader"]), deleteTask);
+router.post("/tasks/add", xRequestId, verifyJWT, checkRole(["leader"]), createTask);
+router.get("/task/:taskId", xRequestId, verifyJWT, checkRole(["leader"]), leaderViewCreatedTask);
+router.get("/tasks", xRequestId, verifyJWT, checkRole(["leader"]), leaderViewAllCreatedTasks);
+router.put("/tasks/edit/:taskId", xRequestId, verifyJWT, checkRole(["leader"]), updateTask);
+router.delete("/tasks/delete/:taskId", xRequestId, verifyJWT, checkRole(["leader"]), deleteTask);
 
 module.exports = router;
